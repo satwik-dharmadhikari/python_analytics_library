27ee2454cb3e7753c6e540bbf63fa127b1db0e4b
Carlos Ariel Gimenez <scire1905@gmail.com>
2016-11-04 15:33:09 -0300

    Inicio Repo

---
+++ get_and_clean/gc_miss.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+
+def missing_values_table(df): 
+        mis_val = df.isnull().sum()
+        mis_val_percent = 100 * df.isnull().sum()/len(df)
+        mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
+        mis_val_table_ren_columns = mis_val_table.rename(
+        columns = {0 : 'Missing Values', 1 : '% of Total Values'})
+        return mis_val_table_ren_columns 
+
+# print(missing_values_table(df))
