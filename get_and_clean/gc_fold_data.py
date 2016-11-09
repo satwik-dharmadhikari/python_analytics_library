8938b0466b00da35ae82f2647132ddd6b2411583
Carlos Ariel Gimenez <scire1905@gmail.com>
2016-11-09 17:38:29 -0300

    Add split

--- get_and_clean/gc_fold_data.py
+++ get_and_clean/gc_fold_data.py
@@ -1,15 +1,17 @@
-import pandas as pd
-
 # Para manipular archivos grandes
 
+import contextlib
 
-def missing_values_table(df): 
-        mis_val = df.isnull().sum()
-        mis_val_percent = 100 * df.isnull().sum()/len(df)
-        mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
-        mis_val_table_ren_columns = mis_val_table.rename(
-        columns = {0 : 'Missing Values', 1 : '% of Total Values'})
-        return print(mis_val_table_ren_columns.sort_values('% of Total Values', ascending=False))
 
-def miss_val_rows(df):
-		sum(map(any, df.isnull()))
+chunksize = 50*10**4
+fid = 1
+with open('IP_BASE_PORT_IN_TRAIN_F.txt', 
+	encoding='cp1252') as infile:
+    f = open('file%d.txt' %fid, 'w')
+    for i,line in enumerate(infile):
+        f.write(line)
+        if not i%chunksize:
+            f.close()
+            fid += 1
+            f = open('file%d.txt' %fid, 'w')
+    f.close()
