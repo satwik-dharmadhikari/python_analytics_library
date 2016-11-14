6bce2828ef91bdfed385c47be7b14277f8233c6b
Carlos Ariel Gimenez <scire1905@gmail.com>
2016-11-14 08:58:13 -0300

    Change to github

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -51,7 +51,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'PyTL'
+project = 'PyAL'
 copyright = '2016, Carlos A. Giménez'
 author = 'Carlos A. Giménez'
 
